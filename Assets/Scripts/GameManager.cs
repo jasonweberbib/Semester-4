0375950c6a62ac0f45347169f0499908356e2604
jasonweberbib <Jason_Elias.Weber@edu.bib.de>
2025-05-06 15:45:55 +0200

    d

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

5009ae941db0982aaa6b5deefdc16b4ec802ddc8
jasonweberbib <Jason_Elias.Weber@edu.bib.de>
2025-05-07 13:50:34 +0200

    Player added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        NetworkManager.Singleton.StartClient();
+        NetworkManager.Singleton.StartHost();
     }
 
     // Update is called once per frame
--- Assets/Scripts/NetPlayer.cs
+++ Assets/Scripts/NetPlayer.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class NetPlayer : MonoBehaviour
+public class NetPlayer : NetworkBehaviour
 {
+    public NetworkVariable<int> score = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+    private float scoreAccumulator = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,33 @@ public class NetPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!IsOwner || !IsSpawned) return;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (x,0,z);
+        if (movement.magnitude > 0)
+        {
+            transform.position += movement * Time.deltaTime;
+            MovingServerRPC(movement);
+        }
+        if ((scoreAccumulator += Time.deltaTime) >= 1.0f)
+        {
+            score.Value += 1;
+            scoreAccumulator -= 1.0f;
+        }
+    }
+
+    [ServerRpc]
+    void MovingServerRPC(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    [ClientRpc]
+    void MovingClientRPC(Vector3 position)
+    {
+        if (IsOwner) return;
+        transform.position = position;
     }
 }
